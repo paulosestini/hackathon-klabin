ed0e832a1ca3a8ab11e20e8e91eac016ff175b35
Miho <miho.urashima@gmail.com>
2020-09-23 20:53:10 -0300

    flip-card

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ trees = [
     Tree(0, "Teste 1", "No mundo atual, o acompanhamento das preferências de consumo nos obriga à análise dos relacionamentos verticais entre as hierarquias. Por outro lado, a valorização de fatores subjetivos é uma das consequências do fluxo de informações. A prática cotidiana prova que a revolução dos costumes pode nos levar a considerar a reestruturação do sistema de participação geral."),
     Tree(1, "Teste 2", "Percebemos, cada vez mais, que a consulta aos diversos militantes estende o alcance e a importância das diversas correntes de pensamento. Por outro lado, o desafiador cenário globalizado acarreta um processo de reformulação e modernização das novas proposições. A prática cotidiana prova que a revolução dos costumes faz parte de um processo de gerenciamento dos níveis de motivação departamental."),
     Tree(2, "Teste 3", "O cuidado em identificar pontos críticos na constante divulgação das informações desafia a capacidade de equalização dos relacionamentos verticais entre as hierarquias. Assim mesmo, o desafiador cenário globalizado pode nos levar a considerar a reestruturação do retorno esperado a longo prazo. No mundo atual, a complexidade dos estudos efetuados cumpre um papel essencial na formulação do orçamento setorial. Caros amigos, o acompanhamento das preferências de consumo representa uma abertura para a melhoria do investimento em reciclagem técnica."),
-] 
+    Tree(3, "Teste 4", "No mundo atual, o acompanhamento das preferências de consumo nos obriga à análise dos relacionamentos verticais entre as hierarquias. Por outro lado, a valorização de fatores subjetivos é uma das consequências do fluxo de informações. A prática cotidiana prova que a revolução dos costumes pode nos levar a considerar a reestruturação do sistema de participação geral."),
+    Tree(4, "Teste 5",
+         "Percebemos, cada vez mais, que a consulta aos diversos militantes estende o alcance e a importância das diversas correntes de pensamento. Por outro lado, o desafiador cenário globalizado acarreta um processo de reformulação e modernização das novas proposições. A prática cotidiana prova que a revolução dos costumes faz parte de um processo de gerenciamento dos níveis de motivação departamental."),
+    Tree(5, "Teste 70",
+         "O cuidado em identificar pontos críticos na constante divulgação das informações desafia a capacidade de equalização dos relacionamentos verticais entre as hierarquias. Assim mesmo, o desafiador cenário globalizado pode nos levar a considerar a reestruturação do retorno esperado a longo prazo. No mundo atual, a complexidade dos estudos efetuados cumpre um papel essencial na formulação do orçamento setorial. Caros amigos, o acompanhamento das preferências de consumo representa uma abertura para a melhoria do investimento em reciclagem técnica.")
+
+]
 
 @app.route('/arvores')
 def page_arvores():
